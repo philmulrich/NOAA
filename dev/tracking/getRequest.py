d015728726f83db386bb735afdc423744814451a
philmulrich <philmulrich@gmail.com>
2020-01-26 23:53:06 +0545

    Added RADIOPASSES to api python script

--- dev/tracking/getRequest.py
+++ dev/tracking/getRequest.py
@@ -1,7 +1,17 @@
 import json
 import requests
 satID=input("NORAD ID: ")
-r=requests.get('https://www.n2yo.com/rest/v1/satellite/tle/'+satID+'/&apiKey=YUYMGA-T96SHL-QFVKMQ-4AB5')
-print(r.content)
+apiKey='&apiKey=YUYMGA-T96SHL-QFVKMQ-4AB5'
+tle = 'https://www.n2yo.com/rest/v1/satellite/tle/'
+radioPasses = 'https://www.n2yo.com/rest/v1/satellite/radiopasses/'
+coords = '/38.762280/-104.834110/1968/'
+days=input("Prediction out to how many days: ")
+minAlt=input("Minimum MaxAltitude in Degrees: ")
+r=requests.get(tle+satID+apiKey)
+tleResponse=r.content
+r=requests.get(radioPasses+satID+coords+days+'/'+minAlt+'/'+apiKey)
+passResponse=r.content
+print(passResponse)
+
 
 
